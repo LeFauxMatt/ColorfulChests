70494fd2f7d91f89d1e75e1f5b29e96518031a6c
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2024-12-30 16:08:46 -0800

    Bump version to 1.0.1-beta.1 and add RemoveHandler method
    - Updated version number in ColorfulChests.csproj to 1.0.1-beta.1 for a new beta release.
    - Added `RemoveHandler` method in ModApi to allow removal of PaletteHandlers.
    - Updated commit hash for the FauxCore subproject.

--- ColorfulChests/ModApi.cs
+++ ColorfulChests/ModApi.cs
@@ -7,4 +7,7 @@ public sealed class ModApi(List<PaletteHandler> handlers) : IColorfulChestsApi
 {
     /// <inheritdoc />
     public void AddHandler(PaletteHandler handler) => handlers.Add(handler);
+
+    /// <inheritdoc />
+    public void RemoveHandler(PaletteHandler handler) => handlers.Remove(handler);
 }
